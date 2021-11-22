0c73b8d5f3a761a84a0835de16741ab7b08df826
Yogeshwaran K <yogeshwaran9600397324@gmail.com>
2021-11-22 15:43:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoList';
+
+  constructor() {}
+
+  inputToDo: any = '';
+  showError: any = '';
+  toDoList: any = [
+    {
+      name: 'First ToDo',
+      completed: false
+    }
+  ];
+  removedToDo: any = [];
+
+  addToDo() {
+    let isHaveFlag: boolean = false;
+    if (this.inputToDo !== ''){
+      this.toDoList.forEach((each: any) => {
+        if (each.name === this.inputToDo) {
+          isHaveFlag = true;
+        } else {
+          isHaveFlag = false;
+        }
+      });
+      if (isHaveFlag) {
+        this.showError = `${this.inputToDo} is alraedy in List.`;
+      } else {
+        this.toDoList.push({
+          name: this.inputToDo,
+          completed: false
+        });
+      }
+    } else {
+      this.showError = 'Enter to Add ToDo List.';
+    }
+    this.inputToDo = '';
+  }
+
+  onclickDone(id: any) {
+    this.toDoList.map((each: any, index: any) => {
+      if ( index == id) {
+        each.completed = !each.completed;
+      }
+      return each;
+    })
+  }
+
+  deleteToDo(id: any) {
+    this.removedToDo = this.toDoList.filter((each: any, index: any) => id !== index);
+    this.toDoList = this.removedToDo;
+    this.removedToDo = [];
+  }
+
+  removeParaTag() {
+    this.showError = ''
+  }
 }
